cc6bf86111c59cd9be5e162cba413ae125cae9a6
Harendra Rawat <yt2022a3@gmail.com>
2026-04-29 21:14:18 +0530

    Update API base URL to production endpoint

--- src/services/axiosconfig.js
+++ src/services/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "https://blogging-app-backend-uvia.onrender.com//api/",
   withCredentials: true
 });
