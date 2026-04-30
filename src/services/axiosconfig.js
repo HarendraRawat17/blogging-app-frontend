c28d258c6ddf0e9229c8e4e081870a78e01b2ff9
HarendraRawat17 <yt2022a3@gmail.com>
2026-04-30 20:23:17 +0530

    basic changes

--- src/services/axiosconfig.js
+++ src/services/axiosconfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  // baseURL: "http://localhost:3001/api/"
+   baseURL: "https://blogging-app-backend-uvia.onrender.com/api/",
   withCredentials: true
 });
