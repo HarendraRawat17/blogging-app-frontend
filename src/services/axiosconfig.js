1bf447e6520cfc6b2afef79526b23a33d93d0024
Harendra Rawat <yt2022a3@gmail.com>
2026-04-30 11:48:21 +0530

    Fix baseURL by removing extra slash

--- src/services/axiosconfig.js
+++ src/services/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://blogging-app-backend-uvia.onrender.com//api/",
+  baseURL: "https://blogging-app-backend-uvia.onrender.com/api/",
   withCredentials: true
 });
